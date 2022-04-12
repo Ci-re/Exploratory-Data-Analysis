eba4ab0954005bed908fd91713c0cecb490bcb3b
Ci-re <cirecleen@gmail.com>
2022-04-12 09:20:39 +0100

    Update

---
+++ Data-wrangling.R
@@ -0,0 +1,69 @@
+getwd()
+
+## Introduction to R for statistical analysis in plant breeding
+
+trial101 = data.frame(
+  variety = c("G01-US234", "G05-BT456", "Ind01", "G11-DR234", "check") , 
+  yield = c(6323.3, 2515.2,5611, 7729, 7843.25),
+  height = c(123.30, 95.2, 113, 89.45, 145.67)
+)
+
+trial101
+
+## Check column names
+names(trial101)
+colnames(trial101)
+
+#Select specified columns and rows
+trial101[c(1,2), c(2,3)]
+
+## Extract specific column from a data frame
+trial101$yield
+
+## Finding the mean of a column
+mean(trial101$yield)
+
+##  Adding new columns to a data frame
+trial101$flowering = c(87, 101, 88, 120, 90)
+
+## importing tidyverse
+library(tidyverse)
+
+## Check out the structure of your data frame
+str(trial101)
+
+## change data frame trial101 to a Tibble
+trial101.new = as_tibble(trial101)
+trial101.new
+
+## importing excel and csv files into R
+mydata = read_csv("heart-disease.csv")
+mydata
+
+library(readxl)
+mydata2 = read_excel("18ayt5USavannahKYK.xls", sheet = 1)
+mydata2
+names(mydata2)
+
+mydata2$clones
+
+## Using the pipe %>% and filter function
+mydata2.obama = mydata2 %>% filter(clones == "OBAMA")
+mydata2.obama
+head(mydata2.obama)
+
+
+## Using the arrange function
+mydata2.arrange = mydata2 %>% arrange(dm)
+View(mydata2.arrange)
+mydata2.arrange2 = mydata2 %>% arrange(clones, environ, dm)
+View(mydata2.arrange2)
+
+## Using the select function, just like filtering
+## Filtering is by rows, while selecting is by columns
+
+mydata2.short = mydata2 %>% select(clones, trial_type, yield, dm)
+mydata2.short
+
+## Using select function to position a variable to the start of
+## the end of a table.
--- data_import.R
+++ data_import.R
@@ -3,5 +3,23 @@ library(dplyr)
 library(tidyr)
 library(Hmisc)
 library(funModeling)
-ag_data = read.csv("20NCRP12YRT_AG.csv")
+library(readxl)
 
+ag_data = read_excel("20NCRP12YRT_AG.xls",
+                     sheet = 1, na = c("NA", " ", "."))
+View(ag_data)
+colnames(ag_data)[c(53,54)] = c("frshwt", "dm")
+
+ag_data = data.frame(ag_data)
+View(ag_data)
+
+for(i in 1:8){
+  ag_data[,i] = as.factor(ag_data[,i])
+}
+
+for(i in 9:54){
+  ag_data[,i] = as.numeric(ag_data[,i])
+}
+
+ag_data %>% ggplot(aes(x = block_number, y = tc)) + geom_boxplot() + 
+  theme(axis.text.x = element_text(angle = 75, hjust = 1, vjust = 1))
